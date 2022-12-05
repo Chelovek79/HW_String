2b802e63713671d0e29eb7fa1c84ef34c4c0a8c2
Vit <privit@rambler.ru>
2022-12-05 23:43:05 +0300

    Задания выполнены.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+// Задание 1.
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println("-----------------------------------------");
+
+// Задание 2.
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println(fullNameUpper);
+
+        System.out.println("-----------------------------------------");
+
+// Задание 3.
+        fullName = "Иванов Семён Семёнович";
+        String fullNameReplace = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullNameReplace);
     }
 }
